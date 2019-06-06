99adbacb2dedfe87043c255ea7eea394a6a5d9fe
Dirk Faust <dif@crosscan.com>
2019-06-06 10:47:18 +0200

    Resolve all dependencies
    ...and put them into an own set to better keep an eye on them as these are only needed for logging

--- CMake/sdk/nRF5_SDK_15.2.0_9412b96.cmake
+++ CMake/sdk/nRF5_SDK_15.2.0_9412b96.cmake
@@ -1,11 +1,24 @@
+set(DEPENDENCIES_LOGGING_SOURCE_FILES
+    "${SDK_ROOT}/components/libraries/balloc/nrf_balloc.c"
+    "${SDK_ROOT}/components/libraries/memobj/nrf_memobj.c"
+    "${SDK_ROOT}/components/libraries/log/src/nrf_log_frontend.c"
+    "${SDK_ROOT}/components/libraries/strerror/nrf_strerror.c"
+)
+
+set(DEPENDENCIES_LOGGING_INCLUDE_DIRS
+    "${SDK_ROOT}/external/fprintf"
+    "${SDK_ROOT}/components/libraries/balloc"
+    "${SDK_ROOT}/components/libraries/ringbuf"
+    "${SDK_ROOT}/components/libraries/memobj")
+
 set(nRF5_SDK_15.2.0_9412b96_SOURCE_FILES
+    ${DEPENDENCIES_LOGGING_SOURCE_FILES}
     "${SDK_ROOT}/components/libraries/util/app_error.c"
     "${SDK_ROOT}/components/libraries/util/app_util_platform.c")
 
 set(nRF5_SDK_15.2.0_9412b96_INCLUDE_DIRS
+    ${DEPENDENCIES_LOGGING_INCLUDE_DIRS}
     "${SDK_ROOT}/integration/nrfx"
-    "${SDK_ROOT}/components/libraries/balloc"
-    "${SDK_ROOT}/components/libraries/memobj"
     "${SDK_ROOT}/components/libraries/util"
     "${SDK_ROOT}/components/libraries/timer"
     "${SDK_ROOT}/components/libraries/log"
